9b8d4bfdea11193b87ef0f4fe30a0268ed81f992
NG JIA WEN <87064232+JIAWEN-NG@users.noreply.github.com>
2024-11-05 18:30:52 +0800

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",  // Adjust paths if necessary
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",  // Adjust paths if necessary
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
